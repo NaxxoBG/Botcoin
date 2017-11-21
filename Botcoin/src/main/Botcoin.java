4645daa72062e9da63b776303a122fd3ade5e133
Atanas Latinov <atanas.latinov@yahoo.com>
2017-11-21 19:14:44 +0100

    Minor changes to the Botcoin class

--- Botcoin/src/main/Botcoin.java
+++ Botcoin/src/main/Botcoin.java
@@ -8,25 +8,30 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 
 public class Botcoin {
-	public static Botcoin bot;
+
 	private IDiscordClient client;
 	private static final String TOKEN = "Mzc2Njk4NTExNjgyNTY4MTk0.DOdsSA.ZTecU4669mbMYO3npXo-EoUIU4A";
+	private static Botcoin bot;
+	private ClientBuilder builder;
 
 	private Botcoin() {
-		createAndLoginClient(TOKEN);
+		createClient();
 	}
 
-	private void createAndLoginClient(String token) {
-		ClientBuilder clientBuilder = new ClientBuilder();
-		clientBuilder.withToken(token);
-		clientBuilder.setMaxMessageCacheCount(250);
-		clientBuilder.setMaxReconnectAttempts(3);
-		clientBuilder.online();
-		this.client = clientBuilder.login();
-		CommandHandler cmdHandler = new CommandHandler() {};
+	private void createClient() {
+		builder = new ClientBuilder();
+		builder.withToken(TOKEN);
+		builder.setMaxMessageCacheCount(250);
+		builder.setMaxReconnectAttempts(3);
+		builder.online();
+	}
+
+	public void activate() {
+		this.client = this.builder.login();
+		CommandHandler cmdHandler = new Discord4JHandler(client);
 		cmdHandler.registerCommand(new CheckCoinCmd());
 		cmdHandler.registerCommand(new ClearCmd());
-		client.getDispatcher().registerListener(cmdHandler);
+		this.client.login();
 	}
 
 	public static Botcoin getBotInstance() {
@@ -40,7 +45,7 @@ public class Botcoin {
 		return client;
 	}
 
-	public void setClient(IDiscordClient client) {
-		this.client = client;
-	}
+//	public void setClient(IDiscordClient client) {
+//		this.client = client;
+//	}
 }
--- Botcoin/src/main/BotcoinMain.java
+++ Botcoin/src/main/BotcoinMain.java
@@ -5,7 +5,6 @@ import org.apache.log4j.BasicConfigurator;
 public class BotcoinMain {
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		Botcoin bot = Botcoin.getBotInstance();
-		//bot.getEvDispatcher().registerListener(new AnnotationListeners());
+		Botcoin.getBotInstance().activate();
 	}
 }
