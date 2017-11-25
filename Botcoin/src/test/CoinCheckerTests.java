a1293f59d1d42925049a974e6a002bd2bb94dc1c
Atanas Latinov <atanas.latinov@yahoo.com>
2017-11-25 22:12:57 +0100

    Added test using apache httpcomponent

--- Botcoin/src/test/CoinCheckerTests.java
+++ Botcoin/src/test/CoinCheckerTests.java
@@ -1,8 +1,15 @@
 package test;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import junit.framework.TestCase;
 import main.CoinTicker;
@@ -24,12 +31,20 @@ public class CoinCheckerTests extends TestCase {
 	public void testMonero() throws InterruptedException, ExecutionException, TimeoutException {
 		System.out.println(CoinTicker.checkCoinPrice("monero", "eur"));
 	}
-	
+
 	public void testNeo() throws InterruptedException, ExecutionException, TimeoutException {
 		System.out.println(CoinTicker.checkCoinPrice("neo", "jpy"));
 	}
-	
-	public void testRegex() {
-		System.out.println(Arrays.toString("$check bitcoin usd".split(" ")));
+
+	public void testHttpClient() {
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet btcInfo = new HttpGet("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR");
+		try {
+			CloseableHttpResponse response = client.execute(btcInfo);
+			System.out.println(IOUtils.toString(response.getEntity().getContent(), Charset.defaultCharset()));
+			response.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 }
